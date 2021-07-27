1e3482ab0567fb96e96f8fea1f7afd5de75f34c0
Gulia Isaeva <gulya.isaeva.pls@gmail.com>
2021-07-27 16:46:26 +0300

    Our Project Added

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    prevArrow:
+      '<button class="slider-btn slider-prev"><img src="images/right-arrow.svg"></button>',
+    nextArrow:
+      '<button  class="slider-btn slider-next"><img src="images/right-arrow.svg"></button>',
+  });
+});
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";$(document).ready((function(){$(".slider").slick({prevArrow:'<button class="slider-btn slider-prev"><img src="images/right-arrow.svg"></button>',nextArrow:'<button  class="slider-btn slider-next"><img src="images/right-arrow.svg"></button>'})}));
